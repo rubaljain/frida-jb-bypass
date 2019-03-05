2eb205fb808d985fcdc18074f0bbc9be481e5920
Rubal Jain <rockinrubal@gmail.com>
2019-03-05 15:50:15 +0530

    Add files via upload
    This javascript can be used to bypass the Jailbreak detection on iOS applications.

---
+++ bypass-jailbreak-detection.js
@@ -0,0 +1,38 @@
+if (ObjC.available)
+{
+    try
+    {
+        //Your class name here
+        var className = "JailbreakDetection";
+        //Your function name here
+        var funcName = "isJailbroken";
+        var hook = eval('ObjC.classes.' + className + '["' + funcName + '"]');
+        Interceptor.attach(hook.implementation, {
+          onLeave: function(retval) {
+            console.log("[*] Class Name: " + className);
+            console.log("[*] Method Name: " + funcName);
+            // console.log("\t[-] Type of return value: " + typeof retval);
+            // //console.log(retval.toString());
+        console.log("\t[-] Return Value: " + retval);
+            
+
+            // printType("\nretval type:\t", retval);
+            // printValue("retval value:\t", retval);
+            // console.log("\n*** exiting", target, "***");
+            // console.log("rj")
+        //For modifying the return value
+            newretval = ptr("0x0") //your new return value here
+            retval.replace(newretval)
+            console.log("\t[-] New Return Value: " + newretval)
+          }
+        });
+    }
+    catch(err)
+    {
+        console.log("[!] Exception2: " + err.message);
+    }
+}
+else
+{
+    console.log("Objective-C Runtime is not available!");
+}
